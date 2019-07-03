7bcdeb321d5acea971537b53672456388493f70d
Yuu Shimizu <p@yuushimizu.com>
2019-07-04 03:11:47 +0900

    remove an unnecessary trait

--- src/points.rs
+++ src/points.rs
@@ -2,16 +2,7 @@ use crate::point2d_range_iterator::{Point2DRangeIterator, Point2DRangeIteratorPr
 use euclid;
 use std::ops;
 
-pub trait Point {}
-
-impl<T, U> Point for euclid::TypedPoint2D<T, U> {}
-
-impl<T, U> Point for euclid::TypedPoint3D<T, U> {}
-
-pub trait Points
-where
-    <Self::Iter as Iterator>::Item: Point,
-{
+pub trait Points {
     type Iter: Iterator;
 
     fn points(self) -> Self::Iter;
@@ -21,12 +12,13 @@ where
 /// ```
 /// # use euclid::{Rect, Point2D, Size2D};
 /// # use euclid_ext::Points;
+/// let rect = Rect::new(Point2D::new(10, 20), Size2D::new(3, 2));
 /// assert_eq!(
 ///     vec![
 ///         Point2D::new(10, 20), Point2D::new(11, 20), Point2D::new(12, 20),
 ///         Point2D::new(10, 21), Point2D::new(11, 21), Point2D::new(12, 21)
 ///     ],
-///     Rect::new(Point2D::new(10, 20), Size2D::new(3, 2)).points().collect::<Vec<_>>());
+///     rect.points().collect::<Vec<_>>());
 /// ```
 impl<T: Point2DRangeIteratorPrimitive, U> Points for euclid::TypedRect<T, U> {
     type Iter = Point2DRangeIterator<T, U>;
@@ -40,12 +32,13 @@ impl<T: Point2DRangeIteratorPrimitive, U> Points for euclid::TypedRect<T, U> {
 /// ```
 /// # use euclid::{Box2D, Point2D};
 /// # use euclid_ext::Points;
+/// let box2d = Box2D::new(Point2D::new(10, 20), Point2D::new(13, 22));
 /// assert_eq!(
 ///     vec![
 ///         Point2D::new(10, 20), Point2D::new(11, 20), Point2D::new(12, 20),
 ///         Point2D::new(10, 21), Point2D::new(11, 21), Point2D::new(12, 21)
 ///     ],
-///     Box2D::new(Point2D::new(10, 20), Point2D::new(13, 22)).points().collect::<Vec<_>>());
+///     box2d.points().collect::<Vec<_>>());
 /// ```
 impl<T: Point2DRangeIteratorPrimitive, U> Points for euclid::TypedBox2D<T, U> {
     type Iter = Point2DRangeIterator<T, U>;
@@ -59,12 +52,13 @@ impl<T: Point2DRangeIteratorPrimitive, U> Points for euclid::TypedBox2D<T, U> {
 /// ```
 /// # use euclid::Point2D;
 /// # use euclid_ext::Points;
+/// let range = Point2D::new(10, 20)..Point2D::new(13, 22);
 /// assert_eq!(
 ///     vec![
 ///         Point2D::new(10, 20), Point2D::new(11, 20), Point2D::new(12, 20),
 ///         Point2D::new(10, 21), Point2D::new(11, 21), Point2D::new(12, 21)
 ///     ],
-///     (Point2D::new(10, 20)..Point2D::new(13, 22)).points().collect::<Vec<_>>());
+///     range.points().collect::<Vec<_>>());
 /// ```
 impl<T: Point2DRangeIteratorPrimitive, U> Points for ops::Range<euclid::TypedPoint2D<T, U>> {
     type Iter = Point2DRangeIterator<T, U>;
