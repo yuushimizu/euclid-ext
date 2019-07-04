2401350a5d1d32fff11e5862a0517b80a9ed2508
Yuu Shimizu <p@yuushimizu.com>
2019-07-04 12:45:02 +0900

    remove PointRangeIterator from the root module

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub mod to_point_range;
 mod type_alias;
 
 pub use crate::map::{Map2D, Map3D};
-pub use crate::point_range_iterator::PointRangeIterator;
 pub use crate::points::Points;
 pub use crate::structure::Structure2D;
 pub use crate::to_point_range::ToPointRange;
