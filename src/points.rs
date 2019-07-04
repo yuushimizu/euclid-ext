5aefbe5cba476c6e98b7971a0b72120d15f0f627
Yuu Shimizu <p@yuushimizu.com>
2019-07-04 16:42:49 +0900

    add tests for points

--- src/points.rs
+++ src/points.rs
@@ -1,40 +1,6 @@
 use crate::point_range_iterator::{PointRangeIterator, PointRangeIteratorItem};
 use crate::to_point_range::ToPointRange;
 
-pub trait Points {
-    type Iter: Iterator;
-
-    fn points(self) -> Self::Iter;
-}
-
-/// # Examples
-/// ```
-/// # use euclid::{Rect, Point2D, Size2D};
-/// # use euclid_ext::Points;
-/// let rect = Rect::new(Point2D::new(10, 20), Size2D::new(3, 2));
-/// assert_eq!(
-///     vec![
-///         Point2D::new(10, 20), Point2D::new(11, 20), Point2D::new(12, 20),
-///         Point2D::new(10, 21), Point2D::new(11, 21), Point2D::new(12, 21)
-///     ],
-///     rect.points().collect::<Vec<_>>());
-/// ```
-///
-
-/// # Examples
-/// ```
-/// # use euclid::{Box2D, Point2D};
-/// # use euclid_ext::Points;
-/// let box2d = Box2D::new(Point2D::new(10, 20), Point2D::new(13, 22));
-/// assert_eq!(
-///     vec![
-///         Point2D::new(10, 20), Point2D::new(11, 20), Point2D::new(12, 20),
-///         Point2D::new(10, 21), Point2D::new(11, 21), Point2D::new(12, 21)
-///     ],
-///     box2d.points().collect::<Vec<_>>());
-/// ```
-///
-
 /// # Examples
 /// ```
 /// # use euclid::Point2D;
@@ -47,8 +13,15 @@ pub trait Points {
 ///     ],
 ///     range.points().collect::<Vec<_>>());
 /// ```
-impl<T: ToPointRange> Points
-    for T where T::Point: PointRangeIteratorItem
+pub trait Points {
+    type Iter: Iterator;
+
+    fn points(self) -> Self::Iter;
+}
+
+impl<T: ToPointRange> Points for T
+where
+    T::Point: PointRangeIteratorItem,
 {
     type Iter = PointRangeIterator<T::Point>;
 
---
+++ tests/points.rs
@@ -0,0 +1,130 @@
+use euclid::{TypedBox2D, TypedBox3D, TypedPoint2D, TypedPoint3D, TypedRect, TypedSize2D};
+use euclid_ext::Points;
+
+enum Space {}
+
+type Point2D<T> = TypedPoint2D<T, Space>;
+
+type Point3D<T> = TypedPoint3D<T, Space>;
+
+macro_rules! point_vec {
+    ($(($x:expr, $y:expr, $z:expr)),*) => {
+        vec![$(Point3D::new($x, $y, $z)),*]
+    };
+    ($(($x:expr, $y:expr)),*) => {
+        vec![$(Point2D::new($x, $y)),*]
+    };
+}
+
+#[test]
+fn points_in_range_2d() {
+    assert_eq!(
+        point_vec![(8, 3), (9, 3), (10, 3), (8, 4), (9, 4), (10, 4)],
+        (Point2D::new(8, 3)..Point2D::new(11, 5))
+            .points()
+            .collect::<Vec<_>>()
+    );
+}
+
+#[test]
+fn points_in_box2d() {
+    assert_eq!(
+        point_vec![(5, 10), (6, 10), (7, 10), (5, 11), (6, 11), (7, 11)],
+        TypedBox2D::new(Point2D::new(5, 10), Point2D::new(8, 12))
+            .points()
+            .collect::<Vec<_>>()
+    );
+}
+
+#[test]
+fn points_in_rect() {
+    assert_eq!(
+        point_vec![
+            (10, 13),
+            (11, 13),
+            (12, 13),
+            (13, 13),
+            (10, 14),
+            (11, 14),
+            (12, 14),
+            (13, 14),
+            (10, 15),
+            (11, 15),
+            (12, 15),
+            (13, 15)
+        ],
+        TypedRect::new(Point2D::new(10, 13), TypedSize2D::new(4, 3))
+            .points()
+            .collect::<Vec<_>>()
+    );
+}
+
+#[test]
+fn points_in_range_3d() {
+    assert_eq!(
+        point_vec![
+            (3, 5, 7),
+            (4, 5, 7),
+            (3, 6, 7),
+            (4, 6, 7),
+            (3, 7, 7),
+            (4, 7, 7),
+            (3, 5, 8),
+            (4, 5, 8),
+            (3, 6, 8),
+            (4, 6, 8),
+            (3, 7, 8),
+            (4, 7, 8),
+            (3, 5, 9),
+            (4, 5, 9),
+            (3, 6, 9),
+            (4, 6, 9),
+            (3, 7, 9),
+            (4, 7, 9),
+            (3, 5, 10),
+            (4, 5, 10),
+            (3, 6, 10),
+            (4, 6, 10),
+            (3, 7, 10),
+            (4, 7, 10)
+        ],
+        (Point3D::new(3, 5, 7)..Point3D::new(5, 8, 11))
+            .points()
+            .collect::<Vec<_>>()
+    );
+}
+
+#[test]
+fn points_in_box3d() {
+    assert_eq!(
+        point_vec![
+            (0, 11, 22),
+            (1, 11, 22),
+            (2, 11, 22),
+            (3, 11, 22),
+            (0, 12, 22),
+            (1, 12, 22),
+            (2, 12, 22),
+            (3, 12, 22),
+            (0, 13, 22),
+            (1, 13, 22),
+            (2, 13, 22),
+            (3, 13, 22),
+            (0, 11, 23),
+            (1, 11, 23),
+            (2, 11, 23),
+            (3, 11, 23),
+            (0, 12, 23),
+            (1, 12, 23),
+            (2, 12, 23),
+            (3, 12, 23),
+            (0, 13, 23),
+            (1, 13, 23),
+            (2, 13, 23),
+            (3, 13, 23)
+        ],
+        TypedBox3D::new(Point3D::new(0, 11, 22), Point3D::new(4, 14, 24))
+            .points()
+            .collect::<Vec<_>>()
+    );
+}
